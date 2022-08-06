a62b7621e3f54349bf9b8693e77e58177faa5d7c
DYK42 <yurandk42@gmail.com>
2022-08-06 19:36:21 +0700

    second task

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -1,9 +1,27 @@
-﻿Console.Write("Введите первое число: ");
+﻿//Console.Write("Введите первое число: ");
+//int Number1 = int.Parse(Console.ReadLine());
+//Console.Write("Введите второе число: ");
+//int Number2 = int.Parse(Console.ReadLine());
+
+//if(Number1 > Number2)
+//    Console.Write("Первое число больше второго!");
+//else
+//    Console.Write("Второе число больше первого");
+
+
+// Вторая задача
+Console.Write("Введите первое число: ");
 int Number1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int Number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int Number3 = int.Parse(Console.ReadLine());
 
-if(Number1 > Number2)
-    Console.Write("Первое число больше второго!");
-else
-    Console.Write("Второе число больше первого");
+int max = Number1;
+if(Number2 > max) {
+    max = Number2;
+}
+if(Number3 > max) {
+    max = Number3;
+}
+Console.Write($"Максимальное число из введенных: {max}");
