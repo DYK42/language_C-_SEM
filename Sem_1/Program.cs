c1848d61ae232e5cd443056991fc6f125a532994
DYK42 <yurandk42@gmail.com>
2022-08-06 19:08:59 +0700

    first task

--- Sem_1/Program.cs
+++ Sem_1/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int Number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int Number2 = int.Parse(Console.ReadLine());
+
+if(Number1 > Number2)
+    Console.Write("Первое число больше второго!");
+else
+    Console.Write("Второе число больше первого");
