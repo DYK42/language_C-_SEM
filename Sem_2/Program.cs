47c11574fc4eec9ad2888c709d51a548224bb9f6
DYK42 <yurandk42@gmail.com>
2022-08-10 20:09:15 +0700

    error correction #2 Sem_2_Task_13

--- Sem_2/Program.cs
+++ Sem_2/Program.cs
@@ -20,41 +20,33 @@ Console.Write($"Вторая цифра числа {number}: {secondNumber}");
 Задача 13: 
 Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
 что третьей цифры нет.
-
+*/
 
 Console.Write("Введите число: ");
-string number = Console.ReadLine();
+int number = int.Parse(Console.ReadLine());
 
-int length = number.Length;
-
-if(length < 3) {
+if(number < 100) {
     Console.Write("Третьей цифры нет!");    
 } else {
-    Console.Write($"Третья цифра числа {number}: {number[2]}");
+    //int len = ((int)(Math.Ceiling(Math.Log10(number))));
+    int num3 = -1;
+    //int num3 = ((int)(num % Math.Pow(10, (--len))));
+    //num3 = ((int)(num3 % Math.Pow(10, (--len))));
+    //num3 /= ((int)(Math.Pow(10, (--len))));
+    //Console.WriteLine($"Цифр в числе {number}: {len}");
+        num3 = (number / 100) % 10;
+    Console.Write($"Третья цифра числа {number}: {num3}");
 }
 
-int num = int.Parse(number);
-
-if(num < 100) {
-    Console.Write("Третьей цифры нет!");    
-} else {
-    int len = ((int)(Math.Ceiling(Math.Log10(num))));
-    int num3 = ((int)(num % Math.Pow(10, (--len))));
-    num3 = ((int)(num3 % Math.Pow(10, (--len))));
-    num3 /= ((int)(Math.Pow(10, (--len))));
-    //Console.Write($"Цифр в числе {num}: {len}");
-    Console.Write($"Третья цифра числа {num}: {num3}");
-}
-*/
 
 
 /*
 Задача 15: 
 Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
 и проверяет, является ли этот день выходным.
-*/
 
 Console.Write("Введите цифру дня недели: ");
 int number = int.Parse(Console.ReadLine());
 if(number > 5 && number < 8) Console.Write("Да");
 else Console.Write("Нет");
+*/
