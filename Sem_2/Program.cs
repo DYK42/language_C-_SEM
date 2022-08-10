db8f51fdc786a06ffa3c545e66db5ea07ea498ea
DYK42 <yurandk42@gmail.com>
2022-08-10 14:30:27 +0700

    add Sem_2_Task_13

--- Sem_2/Program.cs
+++ Sem_2/Program.cs
@@ -2,7 +2,8 @@
 Задача 10: 
 Напишите программу, которая принимает на вход трёхзначное число и 
 на выходе показывает вторую цифру этого числа.
-*/
+
+
 Console.Write("Введите трехзначное число: ");
 int number = int.Parse(Console.ReadLine());
 
@@ -13,3 +14,21 @@ if(number / 100 < 1 || number / 100 >= 10) {
 
 int secondNumber = (number % 100) / 10;
 Console.Write($"Вторая цифра числа {number}: {secondNumber}");
+*/
+
+/*
+Задача 13: 
+Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+*/
+
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+int length = number.Length;
+
+if(length < 3) {
+    Console.Write("Третьей цифры нет!");    
+} else {
+    Console.Write($"Третья цифра числа {number}: {number[2]}");
+}
