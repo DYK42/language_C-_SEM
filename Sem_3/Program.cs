288f215c2734d9f050e3dfb688a7ac31369b80de
DYK42 <yurandk42@gmail.com>
2022-08-13 15:18:37 +0700

    add Task 23 for Sem_3

--- Sem_3/Program.cs
+++ Sem_3/Program.cs
@@ -35,7 +35,6 @@ if(number < 10000 || number > 99999) {
 ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
-*/
 
 Console.Write("Введите координату X первой точки: ");
 int x1 = int.Parse(Console.ReadLine());
@@ -53,3 +52,23 @@ int z2 = int.Parse(Console.ReadLine());
 
 double dist = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2) + Math.Pow(z2-z1, 2));
 Console.WriteLine($"Расстояние между двумя точками в 3D: {dist:f3}");
+*/
+
+/*
+Задача 23
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if(number <= 0) {
+    Console.Write("Число должно быть больше нуля!");
+    return;
+}
+
+for(int i=1; i<=number; i++) {
+    Console.Write(Math.Pow(i, 3) + (i<number?", ":""));
+}
