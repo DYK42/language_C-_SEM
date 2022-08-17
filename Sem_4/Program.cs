d9facddae310b09529e477ff686c6bfb13a19f7f
DYK42 <yurandk42@gmail.com>
2022-08-17 14:36:27 +0700

    add method Task 25 Sem_4

--- Sem_4/Program.cs
+++ Sem_4/Program.cs
@@ -10,10 +10,15 @@ Console.Write("Введите число первое число: ");
 int num1 = int.Parse(Console.ReadLine());
 Console.Write("Введите число второе число: ");
 int num2 = int.Parse(Console.ReadLine());
-int num3 = num1;
 
-for (int i = 0; i < num2 - 1; i++)
+int GetPowerNumber(int a, int b)
 {
-    num3 *= num1;
+    int num = a;
+    for (int i = 0; i < b - 1; i++)
+    {
+        num *= a;
+    }
+    return num;
 }
-Console.Write($"Число {num1} в степени {num2} равно: {num3}");
+
+Console.Write($"Число {num1} в степени {num2} равно: {GetPowerNumber(num1, num2)}");
