f449049df584224079f8eefe3ca6f0634773d679
DYK42 <yurandk42@gmail.com>
2022-08-24 20:51:02 +0700

    add Task *** Sem_6

--- Sem_6/Program.cs
+++ Sem_6/Program.cs
@@ -87,7 +87,7 @@ x*k1 - x*k2 = b2 - b1
 x*(k1-k2) = (b2-b1)
 x = (b2-b1) / (k1-k2)
 */
-
+/*
 Console.Write("Введите число b1: ");
 double b1 = double.Parse(Console.ReadLine());
 Console.Write("Введите число k1: ");
@@ -108,3 +108,94 @@ double y = 0;
 
 GetCross(b1, k1, b2, k2, ref x, ref y);
 Console.Write($"b1 = {b1}, k1 = {k1}, b2 = {b2}, k2 = {k2} --> ({x}; {y})");
+*/
+
+/*
+Задача со звездочкой:
+Напишите программу, которая реализует обход введенного двумерного массива, 
+начиная с крайнего нижнего левого элемента против часовой стрелки.
+
+1 2 3
+4 5 6 -> 7 8 9 6 3 2 1 4 5
+7 8 9
+*/
+
+int[,] GetArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] arr = new int[rows, columns];
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(minValue, maxValue);
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+    return arr;
+}
+
+int[] Spiral(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
+    int[] arr1 = new int[rows * columns];
+    int count = 0;
+    int direction = 0;
+    int start_rows = 0;
+    int start_columns = 0;
+
+    while (start_columns < columns - 1 || start_rows < rows - 1)
+    {
+        if (direction == 0)
+        {
+            for (int i = start_columns; i < columns; i++)
+            {
+                arr1[count] = arr[rows - 1, i];
+                count++;
+            }
+            direction = 1;
+            rows--;
+        }
+        if (direction == 1)
+        {
+            for (int i = rows - 1; i >= start_rows; i--)
+            {
+                arr1[count] = arr[i, columns - 1];
+                count++;
+            }
+            direction = 2;
+            columns--;
+        }
+        if (direction == 2)
+        {
+            for (int i = columns - 1; i >= start_columns; i--)
+            {
+                arr1[count] = arr[start_rows, i];
+                count++;
+            }
+            direction = 3;
+            start_rows++;
+        }
+        if (direction == 3)
+        {
+            for (int i = start_rows; i < rows; i++)
+            {
+                arr1[count] = arr[i, start_columns];
+                count++;
+            }
+            direction = 0;
+            start_columns++;
+        }
+    }
+
+    Console.WriteLine();
+    Console.Write($"{string.Join(", ", arr1)}");
+
+    return arr1;
+}
+
+int[,] array = GetArray(6, 7, 11, 99);
+int[] array1 = Spiral(array);
