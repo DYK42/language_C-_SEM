fcbd44f420ab92edd6a68930f762c75acf6747be
DYK42 <yurandk42@gmail.com>
2022-09-03 15:13:37 +0700

    add Task 68 Sem_9

--- Sem_9/Program.cs
+++ Sem_9/Program.cs
@@ -3,7 +3,7 @@
 Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 M = 1; N = 15 -> 120
 M = 4; N = 8. -> 30
-*/
+
 
 int GetSumRecursion(int numStart, int numStop)
 {
@@ -28,3 +28,44 @@ int numStop = int.Parse(Console.ReadLine());
 
 int sum = GetSumRecursion(numStart, numStop);
 Console.Write($"Сумма элементов интервала от {numStart} до {numStop} равна: {sum}");
+*/
+
+/*
+Задача 68:
+Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+Даны два неотрицательных числа m и n.
+m = 2, n = 3 -> A(m,n) = 29
+*/
+
+int GetAkkermanMethod(int m, int n)
+{
+    if(m < 0 || n < 0) return -1;
+
+    if(m == 0) return n + 1;
+    else
+    {
+        if(m > 0)
+        {
+            if(n == 0) return GetAkkermanMethod(m - 1, 1);
+            else return GetAkkermanMethod(m - 1, GetAkkermanMethod(m, n - 1));
+        }
+    }
+
+    return -1;
+}
+
+Console.Write("Введите число m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Ввведите число n: ");
+int n = int.Parse(Console.ReadLine());
+
+int value = GetAkkermanMethod(m, n);
+
+if(value < 0)
+{
+    Console.Write("Должны быть введены неотрицательные целые числа!");
+}
+else
+{
+    Console.Write($"Вычисление функции Аккермана ( где m = {m}; n = {n} ) равно: {value}");
+}
