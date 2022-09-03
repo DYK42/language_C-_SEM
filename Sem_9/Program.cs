e6fe41323681ab387e6ba4315ada867c67094f3a
DYK42 <yurandk42@gmail.com>
2022-09-03 14:36:10 +0700

    add Task 66 Sem_9

--- Sem_9/Program.cs
+++ Sem_9/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿/*
+Задача 66:
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+int GetSumRecursion(int numStart, int numStop)
+{
+    if(numStart == numStop) return numStart;
+    else
+    {
+        if(numStart > numStop)
+        {
+            int temp = numStop;
+            numStop = numStart;
+            numStart = temp;
+        }
+    }
+    
+    return  numStart + GetSumRecursion(numStart + 1, numStop);
+}
+
+Console.Write("Введите число начала интервала для подсчета суммы элементов: ");
+int numStart = int.Parse(Console.ReadLine());
+Console.Write("Ввведите число окончания интервала для подсчета суммы элементов: ");
+int numStop = int.Parse(Console.ReadLine());
+
+int sum = GetSumRecursion(numStart, numStop);
+Console.Write($"Сумма элементов интервала от {numStart} до {numStop} равна: {sum}");
